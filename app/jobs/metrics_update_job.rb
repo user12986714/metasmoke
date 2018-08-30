b13de824dd58217d673e68967da90a5508406462
izwick-schachter <isaiah@developingtechnician.com>
2018-08-30 18:41:09 -0400

    Try to revert the job I created

--- app/jobs/metrics_update_job.rb
+++ app/jobs/metrics_update_job.rb
@@ -4,14 +4,14 @@ class MetricsUpdateJob < ApplicationJob
   queue_as :default
 
   def perform(path, db_runtime)
-    route = Rails.sensible_routes.match_for path
-    normalized_path = "#{route.verb} #{route.path}"
-    Rails.logger.info "#{normalized_path} #{db_runtime}"
+    # route = Rails.sensible_routes.match_for path
+    # normalized_path = "#{route.verb} #{route.path}"
+    # Rails.logger.info "#{normalized_path} #{db_runtime}"
 
-    query = QueryAverage.find_or_create_by(path: normalized_path)
-    Rails.logger.info "BEFORE: #{query.counter} #{query.average}"
-    query.average = (query.average * query.counter + db_runtime) / (query.counter += 1)
-    query.save
-    Rails.logger.info "AFTER: #{query.counter} #{query.average}"
+    # query = QueryAverage.find_or_create_by(path: normalized_path)
+    # Rails.logger.info "BEFORE: #{query.counter} #{query.average}"
+    # query.average = (query.average * query.counter + db_runtime) / (query.counter += 1)
+    # query.save
+    # Rails.logger.info "AFTER: #{query.counter} #{query.average}"
   end
 end
