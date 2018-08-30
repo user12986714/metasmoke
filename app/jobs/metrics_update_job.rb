a172441201dfc3cd1e633b0ccff2f9cf79eb0b52
izwick-schachter <isaiah@developingtechnician.com>
2018-08-30 19:03:25 -0400

    Logging

--- app/jobs/metrics_update_job.rb
+++ app/jobs/metrics_update_job.rb
@@ -4,14 +4,18 @@ class MetricsUpdateJob < ApplicationJob
   queue_as :default
 
   def perform(path, db_runtime)
-    # route = Rails.sensible_routes.match_for path
-    # normalized_path = "#{route.verb} #{route.path}"
-    # Rails.logger.info "#{normalized_path} #{db_runtime}"
+    @logger ||= Logger.new(File.join(Rails.root, "logs", "metrics_update_job_errors.log"))
+    route = Rails.sensible_routes.match_for path
+    normalized_path = "#{route.verb} #{route.path}"
+    Rails.logger.info "#{normalized_path} #{db_runtime}"
 
-    # query = QueryAverage.find_or_create_by(path: normalized_path)
-    # Rails.logger.info "BEFORE: #{query.counter} #{query.average}"
-    # query.average = (query.average * query.counter + db_runtime) / (query.counter += 1)
-    # query.save
-    # Rails.logger.info "AFTER: #{query.counter} #{query.average}"
+    query = QueryAverage.find_or_create_by(path: normalized_path)
+    Rails.logger.info "BEFORE: #{query.counter} #{query.average}"
+    query.average = (query.average * query.counter + db_runtime) / (query.counter += 1)
+    query.save
+    Rails.logger.info "AFTER: #{query.counter} #{query.average}"
+  rescue => e
+    @logger.error e.message
+    e.backtrace.each { |line| @logger.error line }
   end
 end
