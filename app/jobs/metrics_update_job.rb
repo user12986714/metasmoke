b015a4fb5e0d57cfd1a127c1dd70970b510dbe45
izwick-schachter <isaiah@developingtechnician.com>
2018-08-30 20:36:01 -0400

    Even more fun with logging\!

--- app/jobs/metrics_update_job.rb
+++ app/jobs/metrics_update_job.rb
@@ -7,20 +7,26 @@ class MetricsUpdateJob < ApplicationJob
     @logger ||= Logger.new(File.join(Rails.root, 'log', 'metrics_update_job_errors.log'))
     puts "Is sesnsible routes timing out?"
     route = Rails.sensible_routes.match_for path
-    puts "Maybe!"
+    puts "Checkpoint 1"
     return if route.nil?
     normalized_path = "#{route.verb} #{route.path}"
+    puts "Checkpoint 2"
     Rails.logger.info "#{normalized_path} #{db_runtime}"
 
     query = QueryAverage.find_or_create_by(path: normalized_path)
+    puts "Checkpoint 3"
     Rails.logger.info "BEFORE: #{query.counter} #{query.average}"
     query.average = (query.average * query.counter + db_runtime) / (query.counter += 1)
+    puts "Checkpoint 4"
     query.save
+    puts "Checkpoint 5"
     Rails.logger.info "AFTER: #{query.counter} #{query.average}"
+    puts "Exited! (no error)"
   rescue => e
     puts e.message
     puts e.backtrace
     @logger.error e.message
     e.backtrace.each { |line| @logger.error line }
+    puts "Exited! (error)"
   end
 end
