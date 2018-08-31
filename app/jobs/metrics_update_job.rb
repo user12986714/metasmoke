0101c5a0bcc1d82ff8931a56eb87391344d48c44
izwick-schachter <isaiah@developingtechnician.com>
2018-08-30 21:27:45 -0400

    Last try

--- app/jobs/metrics_update_job.rb
+++ app/jobs/metrics_update_job.rb
@@ -5,7 +5,7 @@ class MetricsUpdateJob < ApplicationJob
 
   def perform(path, db_runtime)
     number_of_the_day = SecureRandom.base64
-    return if ENV['DISABLE_QUERY_LOGGER']
+    # return if ENV['DISABLE_QUERY_LOGGER']
     @logger ||= Logger.new(File.join(Rails.root, 'log', 'metrics_update_job_errors.log'))
     puts 'Is sesnsible routes timing out?'
     route = Rails.sensible_routes.match_for path
@@ -21,7 +21,7 @@ class MetricsUpdateJob < ApplicationJob
     query.average = (query.average * query.counter + db_runtime) / (query.counter += 1)
     puts "#{number_of_the_day} Checkpoint 4"
     # https://stackoverflow.com/questions/24270994/sudden-inexplicable-active-record-connection-timeouts-while-testing-with-rspec
-    ActiveRecord::Base.connection_pool.with_connection do |_conn|
+    Rails.application.executor.wrap do
       query.save
     end
     puts "#{number_of_the_day} Checkpoint 5"
